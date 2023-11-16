cc00badc1d12c0a4e1af002b1ace9b3dd9446681
John Distasio <john@idiotvilla.xyz>
2023-11-16 18:27:47 -0500

    Streaming response

--- src/index.ts
+++ src/index.ts
@@ -27,43 +27,118 @@ export interface Env {
   // MY_QUEUE: Queue;
 }
 
-const methodNotAllowed = (request: Request, allowed: string) => {
-  return new Response(`${request.method} is not allowed`, {
-    status: 405,
-    headers: {
-      Allow: allowed,
-    },
-  });
+const pasteItemHTML = `<!DOCTYPE html>
+<body>
+  <center><h1>Paste Item</h1></center>
+</body>
+`;
+
+const getItemPrefaceHTML = `<!DOCTYPE html>
+<body>
+  <center><h1>Get Item</h1></center>
+  <p>
+`;
+
+const getItemTrailingHTML = `  </p>
+</body>
+`;
+
+const missingItemHTML = `<!DOCTYPE html>
+<body>
+  <center><h1>Missing Item</h1></center>
+</body>
+`;
+
+const methodNotAllowedHTML = `<!DOCTYPE html>
+<body>
+  <center><h1>405 Method Not Allowed</h1></center>
+</body>
+`;
+
+const streamGetItemResponse = async (readable: ReadableStream, writable: WritableStream) => {
+  const encoder = new TextEncoder();
+  const reader = readable.getReader();
+  const writer = writable.getWriter();
+
+  await writer.write(encoder.encode(getItemPrefaceHTML));
+
+  while (true) {
+    const { done, value } = await reader.read();
+
+    if (done) {
+      break;
+    }
+
+    await writer.write(value);
+  }
+
+  await writer.write(encoder.encode(getItemTrailingHTML));
+
+  await writer.close();
 };
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url);
-    const key = url.pathname.slice(1);
+    switch (request.method) {
+      case 'GET':
+        const key = new URL(request.url).pathname.slice(1);
 
-    if (!key) {
-      if (request.method != 'POST') {
-        return methodNotAllowed(request, 'POST');
-      }
+        if (key) {
+          const object = await env.CONTENT.get(key);
 
-      const object = await env.CONTENT.put(crypto.randomUUID(), request.body, {
-        customMetadata: {
-          'cf-connecting-ip': request.headers.get('cf-connecting-ip') || '',
-          'cf-ray': request.headers.get('cf-ray') || '',
-        },
-      });
+          if (!object) {
+            return new Response(missingItemHTML, {
+              headers: {
+                'content-type': 'text/html;charset=UTF-8',
+              },
+              status: 404,
+            });
+          }
 
-      console.log(`created ${object?.key}, ${object?.size} bytes`);
+          const { readable, writable } = new TransformStream();
 
-      return new Response(`${url.host}/${object?.key}`, { status: 201 });
+          // This is specifically _not_ awaited on so the runtime begins sending the
+          // response back as soon as possible.
+          streamGetItemResponse(object.body, writable);
+
+          return new Response(readable, {
+            headers: {
+              'content-type': 'text/html;charset=UTF-8',
+            },
+            status: 200,
+          });
+        }
+
+        return new Response(pasteItemHTML, {
+          headers: {
+            'content-type': 'text/html;charset=UTF-8',
+          },
+          status: 200,
+        });
+      case 'POST':
+        const object = await env.CONTENT.put(crypto.randomUUID(), request.body, {
+          customMetadata: {
+            'cf-connecting-ip': request.headers.get('cf-connecting-ip') || '',
+            'cf-ray': request.headers.get('cf-ray') || '',
+          },
+        });
+
+        console.log(`created ${object.key}, ${object.size} bytes`);
+
+        return new Response(`{"item":"${object.key}"}`, {
+          headers: {
+            'content-type': 'application/json;charset=UTF-8',
+          },
+          status: 201,
+        });
+      default:
+        return new Response(methodNotAllowedHTML, {
+          headers: {
+            allow: 'GET,POST',
+            'content-type': 'text/html;charset=UTF-8',
+          },
+          status: 405,
+        });
     }
-
-    if (request.method != 'GET') {
-      return methodNotAllowed(request, 'GET');
-    }
-
-    const object = await env.CONTENT.get(key);
-
-    return new Response(object?.body);
   },
 };
